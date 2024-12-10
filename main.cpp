d95941d62ac8133645b5de1428d3fedc3bf0578f
daniiul <dmalakhov2005@gmail.com>
2024-12-09 19:46:22 -0800

    implemented deque and vector parts

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <ctime>
 #include <deque>
+#include <vector>
 
 using namespace std;
 
@@ -10,6 +11,17 @@ struct coffeeNode
     string name;
     string drink;
     coffeeNode* next;
+};
+
+struct muffin
+{
+    string type;
+};
+
+struct bracelet
+{
+    string name;
+    string color;
 };
 
 class coffeeBooth
@@ -85,11 +97,26 @@ int main()
     string names[ARRSIZE] = {"Charles", "Layla", "Daniel", "Maggie", "Colin", "Hillary", "Kiyan", "Pedro", "Anna", "Homer"};
     string drinks[ARRSIZE] = {"Coffee", "Tea", "Water", "Frapuchino", "Caffe Latte", "Hot Coco", "Milk", "Pumpkin Latte", "Fruit Shake", "Ice Cream Shake"};
     string muffins[ARRSIZE] = {"Blueberry", "Strawberry", "Bannana", "Chocolate Chip", "English", "Brownie", "Vanilla", "Regular", "American", "Safeway"};
+    string colors[ARRSIZE] = {"Red", "White", "Blue", "Orange", "Purple", "Light Blue", "Violet", "Teal", "Rainbow", "Black"};
 
-    deque<string> muffinLine;
+    vector<bracelet> braceletLine;
+    for (int i = 0; i < 3; i++)
+    {
+        bracelet temp;
+        temp.name = names[rand() % ARRSIZE];
+        temp.color = colors[rand() % ARRSIZE];
+        braceletLine.push_back(temp);
+    }
+
+    deque<muffin> muffinLine;
+    for (int i = 0; i < 3; i++)
+    {
+        muffin temp;
+        temp.type = muffins[rand() % ARRSIZE];
+        muffinLine.push_back(temp);
+    }
 
     coffeeBooth Line1;
-
     Line1.push_back(names[rand() % ARRSIZE], drinks[rand() %  ARRSIZE]);
     Line1.push_back(names[rand() % ARRSIZE], drinks[rand() %  ARRSIZE]);
     Line1.push_back(names[rand() % ARRSIZE], drinks[rand() %  ARRSIZE]);
@@ -100,13 +127,24 @@ int main()
         int prob = rand() % 100 + 1;
         if (prob <= 50)
         {
+            // coffee line
             Line1.push_back(names[rand() % ARRSIZE], drinks[rand() %  ARRSIZE]);
-            muffinLine.push_back(muffins[rand() % ARRSIZE]);
+            // muffin line
+            muffin temp;
+            temp.type = muffins[rand() % ARRSIZE];
+            muffinLine.push_back(temp);
+            // bracelet line
+            bracelet tempbracelet;
+            tempbracelet.name = names[rand() % ARRSIZE];
+            tempbracelet.color = colors[rand() % ARRSIZE];
+            braceletLine.push_back(tempbracelet);
         }
         Line1.serve_customer();
+        Line1.print();
         if(!muffinLine.empty())
             muffinLine.pop_front();
-        Line1.print();
+        if(!braceletLine.empty())
+            braceletLine.erase(braceletLine.begin());
 
     }
     return 0;
