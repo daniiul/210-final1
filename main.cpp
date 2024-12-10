dbddd7c7163bdeca4f237ae202f89ed9dd989d3c
daniiul <dmalakhov2005@gmail.com>
2024-12-09 18:41:34 -0800

    working on code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+struct coffeeNode
+{
+    string name;
+    string drink;
+    coffeeNode* next;
+};
+
+class linkedlist
+{
+private:
+    coffeeNode* head;
+    coffeeNode*
+
+public:
+    linkedlist()
+    {
+        coffeeNode* head = nullptr;
+    }
+    void push(string n, string d)
+    {
+        coffeeNode* temp = new coffeeNode();
+        coffeeNode->name = n;
+        coffeeNode->drink = d;
+        if ()
+    }
+
+};
 
 int main()
 {
